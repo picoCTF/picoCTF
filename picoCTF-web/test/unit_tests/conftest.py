5ca36629fe39d6b4e1ce4365d32f178876544028
Sven Schlinga <sven.schlinga@gmail.com>
2019-05-16 21:45:37 +0200

    add test mongodb addr, port, dbname

--- picoCTF-web/test/unit_tests/conftest.py
+++ picoCTF-web/test/unit_tests/conftest.py
@@ -9,16 +9,18 @@ import api.config
 import pytest
 from pymongo import MongoClient
 
+TEST_MONGO_ADDR = '127.0.0.1'
+TEST_MONGO_PORT = 27017
+TEST_MONGO_DB_NAME = 'ctf-test'
+
 
 def setup_db():
     """ Creates a mongodb instance and shuts it down after testing has concluded. """
 
-    client = MongoClient(
-        api.config.testing_mongo_addr,
-        api.config.testing_mongo_port)[api.config.testing_mongo_db_name]
+    client = MongoClient(TEST_MONGO_ADDR, TEST_MONGO_PORT)[TEST_MONGO_DB_NAME]
 
     if len(client.collection_names()) != 0:
-        client.connection.drop_database(api.config.testing_mongo_db_name)
+        client.connection.drop_database(TEST_MONGO_DB_NAME)
 
     # Set debug client for mongo
     if api.common.external_client is None:
@@ -29,8 +31,6 @@ def setup_db():
 
 def teardown_db():
     """ Drops the db and shuts down the mongodb instance. """
-    client = MongoClient(
-        api.config.testing_mongo_addr,
-        api.config.testing_mongo_port)[api.config.testing_mongo_db_name]
-    client.connection.drop_database(api.config.testing_mongo_db_name)
+    client = MongoClient(TEST_MONGO_ADDR, TEST_MONGO_PORT)[TEST_MONGO_DB_NAME]
+    client.connection.drop_database(TEST_MONGO_DB_NAME)
     client.connection.disconnect()
